a1020807903a6c9f66d459f87ace217bef80639d
Peter Eckel <pe-git@hindenburgring.com>
2026-08-04 12:54:12 +0200

    Prepared release 0.1.10

--- netbox_dhcp/__init__.py
+++ netbox_dhcp/__init__.py
@@ -2,7 +2,7 @@ from django.utils.translation import gettext_lazy as _
 
 from netbox.plugins import PluginConfig
 
-__version__ = "0.1.9"
+__version__ = "0.1.10"
 
 
 class DHCPConfig(PluginConfig):
